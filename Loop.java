8da3baf96bb2c1b77c582b14787a1f4883942749
Francisco027 <franciscotrn.27@gmail.com>
2019-04-01 19:53:50 -0600

    loop acepted

--- Loop.java
+++ Loop.java
@@ -51,107 +51,106 @@ public class Loop {
     }
     public static void main(String[] args) throws IOException {
         
-        //<editor-fold defaultstate="collapsed" desc="loop">
         
-        Scanner entrada = new Scanner();
-        
-        int muestras = intconvert(entrada.next());
-        
-        
-        int arrDeMuestras[];
-        
-        
-        int contAux = 1;
-        
-        int numInit = 0;
-        int sigNum = 0;
-        int antNum = 0;
-        
-        int salida = 0;
-        
-        boolean sentido = false; /* false abajo */
-        
-        int inicial = 0;
-        int fin = 0;
-        
-        while(muestras != 0)
-        {
-            //antNum = intconvert(entrada.next());
-//            inicial = antNum;
-//            sigNum = intconvert(entrada.next());
-//            //sentido = true;//sube
-//            //salida++; 
-////            if ( sigNum < antNum || sigNum > antNum && muestras == 2)
-////            {
-////                System.out.println("NO DEBE ENTRAR A ESTE BUCLE");
-////                sentido = true;//sube
-////                salida++;
-////                
-////            }
-//            
+//<editor-fold defaultstate="collapsed" desc="loop02 to Send">
+    
+    
+Scanner entrada = new Scanner();
+    
+int siguiente = 0, ant = 0; 
+boolean sentido = true;
+int salida = 0 ; 
 
-            antNum = intconvert(entrada.next());
-            inicial = antNum;
+
+
+int N = intconvert(entrada.next());
+
+//contador de picos se inicializa en dos por que la primera recta se inicializa estaticamente 
+int picos = 2; 
+
+
+
+
+
+    while ( N != 0 ) {            
+        
+        //se hace la primer recta 
+        int pInit = intconvert(entrada.next()), pInit1 = intconvert(entrada.next()) ;
+
             
+
+        //definir el sentido inicial
+        sentido = (pInit > pInit1) ? false : true ;
+
             
+
+        ant = pInit1;
+        
+        while ( picos != N ) 
+        {
             
+            siguiente = intconvert(entrada.next());
+
             
-            
-            while(contAux != muestras)
+
+            if( siguiente > ant && !sentido )
             {
-                /*
-                    si baja o sube
-                */
-                sigNum = intconvert(entrada.next());
                 
-                System.out.println("ant = " + antNum + " Sig = " + sigNum);
                 
-                if(antNum > sigNum && sentido)
-                {
-                    System.out.println("baja p");
-                    salida++;
-                    sentido = false;
-                    System.out.println("sentido queda en = " + sentido);
-                    
                 
-                }else if(antNum < sigNum && !sentido)
-                {
-                    System.out.println("sube p");
-                    salida++;
-                    sentido = true;
-                    System.out.println("sentido queda en = " + sentido);
-                }
                 
-                antNum = sigNum;
-                contAux++;
+                salida++;
+                sentido = true;
+                
+            }else if ( siguiente < ant && sentido  )
+            {
+                
+                
+                
+                
+                salida++;
+                sentido = false;
                 
             }
-            fin = sigNum;
-            
-            System.out.println( " ini " + inicial + " fin = " + fin );
-            //if salida++;
-            
-            if (fin < inicial && !sentido) 
-            {
-                System.out.println("baja fin p");
-                salida++;
-                
-            } else if(fin > inicial && sentido)
-            {
-             
-                System.out.println("sube fin p");
-                salida++;
-                
-            }
-           
-            
-            System.out.println("SALIDA = " + salida);
-            muestras = intconvert(entrada.next());
-            contAux = 1;
+
+            ant = siguiente;
+            picos++;
         }
         
-        //</editor-fold>
+        int pFinal = ( N != 2 ) ? siguiente: pInit1;
         
+        if( pInit > pFinal && !sentido ) 
+        {
+            salida++;
+            sentido = true;
+            
+        }else if( pInit < pFinal && sentido  )
+        {
+            salida++;
+            sentido = false;
+        }
+        
+        if ( pInit1 > pInit && !sentido) {
+            salida++;
+        }else if( pInit1 < pInit && sentido )
+        {
+            salida++;
+        }
+        
+        System.out.println(salida);
+        
+        
+        
+        ant = 0;
+        siguiente = 0;
+        picos = 2 ;
+        sentido = true;
+        salida = 0;
+        N = intconvert(entrada.next());
+    }
+    
+//</editor-fold>    
+   
     }
     
 }
