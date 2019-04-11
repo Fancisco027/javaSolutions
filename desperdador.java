5e5414f5ec2da40d7053a9e406a81ddc9126754c
Francisco027 <franciscotrn.27@gmail.com>
2019-04-10 19:38:38 -0500

    despertador

---
+++ desperdador.java
@@ -0,0 +1,20 @@
+
+
+
+public static void main(String[] args) {
+    
+//<editor-fold defaultstate="collapsed" desc="despertador">
+
+    Scanner entrada = new Scanner();
+
+    int m1 = 0, m2 = 0, h1 = 0 , h3 = 0 ;
+
+    m1
+
+    while (condition) {
+        
+    }
+
+//</editor-fold>
+
+}
