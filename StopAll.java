3578f1332d017a0ff4f5fee9324baec97f88fa9f
Fran027 <franciscotrn.27@gmail.com>
2020-01-30 09:20:48 -0600

    StopAll 5% wrong ans

--- StopAll.java
+++ StopAll.java
@@ -11,10 +11,11 @@ import java.util.*;
 
 /**
  *
- * @author francisco
+ * @author Francisco
  */
 public class StopAll {
-
+    
+    
             
 //<editor-fold defaultstate="collapsed" desc="scanner">
     static class Scanner{
@@ -48,8 +49,6 @@ public class StopAll {
     
     static class Vecindario{
         
-        
-        
         List<Integer> listVertex;
         
         public Vecindario( List<Integer> l ){
@@ -59,118 +58,61 @@ public class StopAll {
             this.listVertex = new ArrayList<Integer>();
         }
         
-        
-        
     }
-    
-    public static Vecindario persistencia = null;
     
     public static int intConvert( String val ){
         return Integer.parseInt( val );
     }
     
-    public static String returnStringArray( Object[] arr )
-    {
-        String salida = "" ; 
-        for (int i = 0; i < arr.length; i++) {
-            salida+= String.valueOf( arr[i] );
-            
-        }
-        return salida;
-    }
     
     // parametros
     // Arreglo (grafo), inicio , nodoObjetivo
     
     public static String letsOrDeur( int nodoInicio, int nodoObjetivo , Vecindario[] l )
         {
+            //nivel o nodo ( en donde se vizualizan los vecinos )
+            int nodoActual = 0;
+            
             int arrVisitados[] = new int[ l.length ];
             
-            int cont = 0;
-            
+            //Queue para visitar todos los nodos
             Queue< Integer > q = new LinkedList< Integer >();
             
-            Set< Integer > setToQueue = new HashSet<Integer>();
-            
+            //primer nodo para iniciar el recorrido
             q.add( nodoInicio );
             
             while( !q.isEmpty() )
             {
-                cont++;
-//                System.out.println("cont = " + cont + "proceso");
-//                System.out.println("Tamanio de QUEUE : " + q.size());
-//                System.out.println("==========================");
-//                System.out.println("Elementos de cola:");
-//                System.out.println( q.toString() );
-//                System.out.println("/////////////////////////////");
-                int nodoActual = q.remove();
+                //se optiene el nodo que se analizara 
+                nodoActual = q.remove();
                 
-                System.out.println("==================");
-                System.out.println( "nodoActial = " + ( nodoActual + 1 ) );
-                if ( l[ nodoActual ] != null )
-                    System.out.println("LISTA > " + l[ nodoActual ].listVertex.toString());
+                //marca como visitado el nodo que se analizara 
                 arrVisitados[nodoActual] = 1;
                 
-                if ( l[ nodoActual ] == null ) {
-//                    Syste'm.out.println("Entro al CONDICION EN ");
-//                    System.out.println( nodoActual );
+                //si es nulo no se toma el siguiente
+                if ( l[ nodoActual ] == null )
                     continue;
-                    //return "Deu ruim";
-                }
                 
-//                System.out.println("==== CONDICION ===");
-//                System.out.println("Se busca : " + nodoObjetivo + " a partir de: " + nodoActual);
-//                System.out.println( l[ nodoActual ].listVertex.indexOf( nodoObjetivo ) );
-//                System.out.println("/.=================");
-                
+                //ciclo de analizis de vecinos 
                 for (int i = 0; i < l[ nodoActual ].listVertex.size() ; i++)
-                {
+                {   
+                    //si se encuentra el nodo objetivo terminar el proceso
                     if ( l[nodoActual].listVertex.get(i) == nodoObjetivo )
-                    {
                         return "Lets que lets";
-                    }
                     
+                   //en caso de que ya se visito, continuar con el siguiente vecino
                     if ( arrVisitados[ l[nodoActual].listVertex.get(i) ] == 1 )
-                    {
                         continue;
-                    }
                     
-                    q.add( arrVisitados[ l[nodoActual].listVertex.get(i) ] );
+                    //se agrega a cola para despues realizar la busqueda en sus vecinos 
+                    q.add( l[nodoActual].listVertex.get(i) );
                     
                 }
 
-
-                //<>
-                //comprobando si existe el nodo objetivo en tal lista objetivo
-//                if ( l[ nodoActual ].listVertex.indexOf( nodoObjetivo ) != -1 ) 
-//                {
-//                    return "Lets que lets";
-//                }else
-//                {
-////                    System.out.println("==== Agrega nodos que no existen, PRE son: ===");
-////                    System.out.println( returnStringArray(  setToQueue.toArray()  )  );
-////                    System.out.println("====/.========================================");
-//                    setToQueue.addAll( q ) ;
-////                    System.out.println("=== Agrega nodos que no existen, POST son: ===");
-////                    System.out.println( returnStringArray(  setToQueue.toArray()  ) );
-////                    System.out.println("====/.========================================");
-////                    
-//                    setToQueue.addAll( l[ nodoActual ].listVertex ) ;
-//                    q.clear();
-//                    q.addAll( setToQueue );
-////                    System.out.println("============ La cola es, POST son: ===========");
-////                    System.out.println( q.toString() );
-////                    System.out.println("tope es: " + q.poll());
-////                    System.out.println("====/.====================================");
-//                    
-//                }
-                
-                
             }
             
             return "Deu ruim";
         }
-    
     public static void main(String[] args) throws IOException {
         
         Scanner entrada = new Scanner();
@@ -192,89 +134,34 @@ public class StopAll {
             ArrAyB[0] = intConvert( entrada.next() );
             ArrAyB[1] = intConvert( entrada.next() );
             
-//            Arrays.sort( ArrAyB );
-            
+            //ingreso de "vecindarios"
             if ( ( arrVecindario[ ArrAyB[0] - 1 ] == null ) ) 
-            {
                 arrVecindario[ ArrAyB[0] - 1 ] = new Vecindario( new ArrayList() );
-                arrVecindario[ ArrAyB[1] - 1 ] = new Vecindario( new ArrayList() );
-            }
             
+            //ingreso de "vecindarios"
             arrVecindario[ ArrAyB[0] - 1 ].listVertex.add( ArrAyB[1] - 1 );
-            arrVecindario[ ArrAyB[1] - 1 ].listVertex.add( ArrAyB[0] - 1 );
-            //para asignacion de valores siempre restar uno
+            
         }
         String salida = "";
         for (int j = 0; j < P; j++) 
         {
-            
+            //consultas 
             ArrKyL[0] = intConvert( entrada.next() );
             ArrKyL[1] = intConvert( entrada.next() );
-//            Arrays.sort( ArrKyL );
             
+            //como es un grafo no dirigido se hace para los 2 lados (*)
+            //(*)
             salida = letsOrDeur( ArrKyL[0] - 1 , ArrKyL[ 1 ] - 1  , arrVecindario );
-            if ( !salida.equals("Lets que lets") ) 
-            {
-                System.out.println(">FIN DE PRIMER PROCESP<");
-                System.out.println("BUELTA 2");
-                salida = letsOrDeur( ArrKyL[1] - 1 , ArrKyL[ 0 ] - 1  , arrVecindario );
-            }
-            System.out.println(salida);
             
+            if ( !salida.equals("Lets que lets") ) 
+                //(*)
+                salida = letsOrDeur( ArrKyL[1] - 1 , ArrKyL[ 0 ] - 1  , arrVecindario );
+            
+            //out
+            System.out.println(salida);
             
         }
 
-        
-        
-//        //<editor-fold defaultstate="collapsed" desc="StopAll con listas">
-//        //se inicializan variables de entrada
-//        int N = intConvert( entrada.next() );
-//        int M = intConvert( entrada.next() );
-//        int P = intConvert( entrada.next() );
-//        
-//        int ArrAyB[] = new int[2];
-//        int ArrKyL[] = new int[2];
-//        Vecindario arrVecindario[] = new Vecindario[N];
-//        
-//        for (int i = 0; i < M; i++) {
-//            ArrAyB[0] = intConvert( entrada.next() );
-//            ArrAyB[1] = intConvert( entrada.next() );
-//            
-//            Arrays.sort( ArrAyB );
-//            
-//            arrVecindario[ ArrAyB[0] - 1 ] = new Vecindario( new ArrayList() );
-//            
-//            arrVecindario[ ArrAyB[0] - 1 ].listVertex.add( ArrAyB[1] );
-//            
-//            
-//            
-//        }
-//        
-//        for (int i = 0; i < P; i++) {
-//            
-//            ArrKyL[0] = intConvert( entrada.next() );
-//            ArrKyL[1] = intConvert( entrada.next() );
-//            Arrays.sort( ArrKyL );
-//            
-//            System.out.println("=========array================");
-//            System.out.println("ArrAyb pos[0] : " + ArrKyL[0] + " pos [1] " + ArrKyL[1]);
-//            System.out.println("==============================");
-//            if( arrVecindario[ ArrKyL[0] ].listVertex.indexOf( ArrKyL[1] ) == -1 ) {
-//                System.out.println("    ==valor=");
-//                //System.out.println("    " + arrVecindario[ ArrKyL[0] ].listVertex.get( ArrKyL[1] ) );
-//                System.out.println("    " + arrVecindario[ ArrKyL[0] ].listVertex.toString() ) ;
-//                System.out.println("    ========");
-//                System.out.println("Deu ruim");                
-//            }
-//
-//            else 
-//                System.out.println("Lets que lets");
-//                
-//                
-//        }
-//        //</editor-fold>
-
-        
     }
     
 }
